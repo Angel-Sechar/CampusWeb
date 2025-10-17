859516445f0c389a46f78f91ea1d62ed09677931
Angel Sechar <asv70537592@gmail.com>
2025-10-16 22:52:17 -0500

    Remove microservices folder to convert to submodule

--- microservices/auth-service/src/main/java/com/ecommerce/campus/authservice/persistence/base/StoredProcedureCaller.java
+++
@@ -1,5 +0,0 @@
-package com.ecommerce.campus.authservice.persistence.base;
-
-public interface StoredProcedureCaller {
-    <T> StoredProcedureBuilder<T> callProcedure(String procedureName, Class<T> resultType);
-}
--- microservices/init/creation-database-v01.sql
+++
@@ -1,8 +0,0 @@
-/*Creation of databases*/
-IF NOT EXISTS (
-    SELECT name FROM sys.databases WHERE name = 'db_auth'
-)
-BEGIN
-    CREATE DATABASE db_auth;
-END
-GO
